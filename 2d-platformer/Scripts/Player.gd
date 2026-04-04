78415afe2cc8c9d2d63fb0908d7799a445d14e17
bartletuni <bartletuni@gmail.com>
2026-04-04 16:01:10 -0600

    Player movement basics

--- 2d-platformer/Scripts/Player.gd
+++ 2d-platformer/Scripts/Player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -11,14 +10,27 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("Jump")
+		await get_tree().create_timer(0.4).timeout
+		$AnimatedSprite2D.play("Idle")
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if velocity.x > 0:
+			$AnimatedSprite2D.play("Walk")
+			$AnimatedSprite2D.flip_h = false
+		elif velocity.x < 0:
+			$AnimatedSprite2D.play("Walk")
+			$AnimatedSprite2D.flip_h = true
+		elif is_on_floor() == false:
+			$AnimatedSprite2D.play("Jump")
+			
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
