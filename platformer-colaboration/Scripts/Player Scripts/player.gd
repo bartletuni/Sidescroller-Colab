50fa8ab494e9a8a6a990a15bb6df97952b44df11
bartletuni <bartletuni@gmail.com>
2026-04-05 14:45:16 -0600

    Some Code For Player
    Basic code for the player

--- platformer-colaboration/Scripts/Player Scripts/player.gd
+++ platformer-colaboration/Scripts/Player Scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+const RUN_SPEED = 400.0
 const JUMP_VELOCITY = -400.0
 
 
@@ -11,14 +12,20 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$Animation.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	var sprint := Input.action_press("run")
 	if direction:
 		velocity.x = direction * SPEED
+		$Animation.play("Walk")
+	elif direction and sprint:
+		velocity.x = direction * SPEED
+		$Animation.play("Walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
