2fbb194c8843a14692309bcf7a59adcd3eab1bab
bartletuni <bartletuni@gmail.com>
2026-04-28 17:19:49 -0600

    Use texture for body light; add light asset
    Add Untitled-1.png assets and import metadata, update player scene to use the new texture for BodyLight (replace gradient), adjust BodyLight properties (energy, texture_scale), and reinsert the Animator AnimatedSprite node. Refactor body_light.gd: add player onready reference, move logic to _physics_process, simplify flashlight handling and make the light look_at the mouse. Also include minor .DS_Store updates.

--- platformer-colaboration/Scripts/Player Scripts/body_light.gd
+++ platformer-colaboration/Scripts/Player Scripts/body_light.gd
@@ -2,28 +2,29 @@ extends PointLight2D
 
 @onready var body_light: PointLight2D = $"."
 @onready var flashlight_ray: RayCast2D = $"../FlashlightRay"
+@onready var player: CharacterBody2D = $".."
 
 var flashlight_on: bool = true
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+	#if flashlight_on == true:
+		#body_light.position.y -= 100
+	pass
+		
+func _physics_process(delta: float) -> void:
 	var mouse_pos = get_global_mouse_position()
 	
 	if Input.is_action_just_pressed("Toggle Flashlight"):
 		toggle_flashlight()
-	if flashlight_on == true:
-		flashlight_ray.target_position = flashlight_ray.to_local(mouse_pos)
-		flashlight_ray.force_raycast_update()
+	#if flashlight_on == true:
+		#body_light.position.y -= 100
+		#flashlight_ray.look_at(mouse_pos)
+		#flashlight_ray.force_raycast_update()
+		#
+		#if flashlight_ray.is_colliding():
+			#body_light.global_position = flashlight_ray.get_collision_point()
 		
-		if flashlight_ray.is_colliding():
-			body_light.global_position = flashlight_ray.get_collision_point()
-		else:
-			body_light.global_position = mouse_pos
+	body_light.look_at(mouse_pos)
 
 
 func toggle_flashlight():
