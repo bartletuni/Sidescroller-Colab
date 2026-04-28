6ed4eba51230fdb2d4c74841d06b2a609ec622e9
bartletuni <bartletuni@gmail.com>
2026-04-28 14:56:06 -0600

    Revert "Refactor crate objects & add ObjectData"
    This reverts commit 218b345431f6455a261820d1781dc95ae3068b2a.

--- platformer-colaboration/Scripts/Object Scripts/PhysicsCrate.gd
+++ platformer-colaboration/Scripts/Object Scripts/PhysicsCrate.gd
@@ -4,15 +4,33 @@ extends RigidBody2D
 
 var fly_left = false
 var fly_right = false
+var left_vec = Vector2(-200, -200)
+var right_vec = Vector2(200, -200)
+
+
 
 func _physics_process(delta: float) -> void:
-	ObjectData.applied_force(wooden_crate, fly_left, fly_right)
+	if fly_left:
+		wooden_crate.apply_central_impulse(left_vec)
+		fly_left = false
+	elif fly_right:
+		wooden_crate.apply_central_impulse(right_vec)
+		fly_right = false
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	var groups = area.get_groups()
-	ObjectData.detect_player_attack(groups, wooden_crate, fly_left, fly_right)
+	if "PlayerAttack" in groups and (wooden_crate.position.x < PlayerData.player_x_position):
+		fly_left = true
+	elif "PlayerAttack" in groups and (wooden_crate.position.x > PlayerData.player_x_position):
+		fly_right = true
 
 
 func _on_enemyattackbox_area_entered(area: Area2D) -> void:
 	var groups = area.get_groups()
-	ObjectData.detect_enemy_attack(groups, wooden_crate, fly_left, fly_right)
+	while EnemyData.within and EnemyData.kicking > 0.65:
+		if "EnemyAttack" in groups and (wooden_crate.position.x < EnemyData.enemy_x_position):
+			fly_left = true
+		elif "EnemyAttack" in groups and (wooden_crate.position.x > EnemyData.enemy_x_position):
+			fly_right = true
+			
+		await get_tree().create_timer(0.3).timeout
--- platformer-colaboration/Scripts/Object Scripts/WoodenCrate.gd
+++ platformer-colaboration/Scripts/Object Scripts/WoodenCrate.gd
@@ -1,18 +1,21 @@
-extends RigidBody2D
+extends StaticBody2D
 
-@onready var wooden_crate: RigidBody2D = $"."
+const HEALTH = 4
 
-var fly_left = false
-var fly_right = false
+var health = HEALTH
 
 func _physics_process(delta: float) -> void:
-	ObjectData.applied_force(wooden_crate, fly_left, fly_right)
+	if health == 4:
+		$Animator.play("Full")
+	elif health == 2:
+		$Animator.play("Broken")
+	elif health < 1:
+		$Animator.play("Destroyed")
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
-	var groups = area.get_groups()
-	ObjectData.detect_player_attack(groups, wooden_crate, fly_left, fly_right)
+	var group = area.get_groups()
+	if "PlayerAttack" in group:
+		health -= 1
 
-
-func _on_enemyattackbox_area_entered(area: Area2D) -> void:
-	var groups = area.get_groups()
-	ObjectData.detect_enemy_attack(groups, wooden_crate, fly_left, fly_right)
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	_on_hitbox_area_entered(area)
